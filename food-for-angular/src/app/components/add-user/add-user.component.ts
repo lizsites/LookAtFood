f31e711748094ea16c911b1eeec70ff5a35247ef
Nicole Lang <nicole.lang@revature.net>
2020-09-13 18:19:30 -0400

    add register

--- food-for-angular/src/app/components/add-user/add-user.component.ts
+++ food-for-angular/src/app/components/add-user/add-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService } from 'src/app/services/register.service';
+import { UseExistingWebDriver } from 'protractor/built/driverProviders';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
+  public username : string;
+  public password : string;
+  constructor(private register : RegisterService) {
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
+  }
+
+  addUserFunc(){
+    
+    let u = new User();
+    u.username = this.username;
+    u.password = this.password;
+    u.id = null;
+    console.log(u);
+    this.register.register(u);
+    this.register.register(u).subscribe((data) => {
+      console.log(data);
+      u = data;
+    }, () => {
+      console.log("No you goofed");
+    })
   }
 
 }
---
+++ food-for-angular/src/app/services/register.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http : HttpClient) { }
+
+  public register(user :User) : Observable<User> {
+    let jsonUser : string = JSON.stringify(user);
+    return this.http.post("http://localhost:8080/JenkinsRepo/user", jsonUser, {
+
+      headers : {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      },
+      withCredentials : true,
+      responseType : "json"
+    }) as Observable<User>;
+  }
+}

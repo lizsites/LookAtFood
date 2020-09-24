928fcbbd930e204d08c48856d2206e0bccf6eea3
Nicole Lang <nicole.lang@revature.net>
2020-09-24 13:07:33 -0400

    validation on number inputs + more search/diet options + working 'edit recipe' button in cookbook

--- food-for-angular/src/app/components/update-user/update-user.component.ts
+++ food-for-angular/src/app/components/update-user/update-user.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
 import { Preference } from 'src/app/models/preference';
 import { Md5 } from 'ts-md5';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -17,44 +18,37 @@ export class UpdateUserComponent implements OnInit {
   diet : string;
   minCalories : number;
   maxCalories : number;
-  passedUsername : string;
+  //passedUsername : string;
   user : User;
-  @Input() username : string;
+  //@Input() username : string;
 
-  constructor(private update : UpdateInfoService, private login : LoginService) { }
+  constructor(private update : UpdateInfoService, private login : LoginService, private route : Router) { }
 
   ngOnInit(): void {
-   this.password = this.login.serviceUser.password;
-  }
-  viewUser(){
-    /*
-    When you come to the update-user component, you should be able to test 
-    if the serviceUser field is working correctly by pressing the "view user "
-    button.
-
-    It should print the user info of the user who logged in.
-
-    Temporarily, this is available while update-user and login components are on
-    the same page, because I do not know how to make a navbar yet.
-    */
-
-    this.user = this.login.serviceUser;
-    console.log(this.user);
+   //fill recipe form
+   this.user = this.login.serviceUser;
+   this.minCalories = this.user.preference.minCalories;
+   this.maxCalories = this.user.preference.maxCalories;
+   this.diet = this.user.preference.dietType;
   }
 
   updateUser(){
-
     let u : User = this.login.serviceUser;
-    u.password = Md5.hashStr(this.password).toString().toUpperCase();
+    if(this.password != null && this.password != ""){
+      u.password = Md5.hashStr(this.password).toString().toUpperCase();
+    }
     u.preference = new Preference(u.preference.id,this.minCalories,this.maxCalories,this.diet);
-    console.log(u);
+    console.log("updated user being sent: "+ u);
+
     this.update.updateUserInfo(u).subscribe((data)=>{
-      console.log("data returned " + data);
-      u = data;
+      console.log("data returned: " + data);
+      this.login.serviceUser = data;
       console.log("update user " + u)
+      this.route.navigate(['search']);
       window.sessionStorage.setItem("user", u.username)}, () =>{
         console.log("error in the update user info");
       }
+
       )
 
   }
